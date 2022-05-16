095e7f3904e1d9a8b21eea9a777649fe721f36a4
Kiran Smelser <kiran.smelser@gmail.com>
2022-05-15 21:11:18 -0600

    Added a feature that prints out the info of an enterred file

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ from pathlib import Path
 
 
 def main():
-    file_name = input("Please enter the .zip filename: ")
+    file_name = input("Please enter the entire filename: ")
     zip_file = zipfile.ZipFile(file_name)
     file_list = zip_file.namelist()
     print()
     # Prints the names of the files/folders in the enterred .zip file.
     print(f"FILES AND FOLDERS IN \"{file_name}\":")
-    for element in file_list[4:]:
-        print(element)
+    for element in file_list:
+        print(f"\t{element}")
+    print()
+    # Prints out info of the enterred file.
+    command = input("Please enter the name of the file/folder you would like info on: ")
+    print()
+    file_info = zip_file.getinfo(command)
+    print("FILE OR FOLDER INFO:")
+    print(f"\tOriginal File Name: {file_info.orig_filename}")
+    print(f"\tDate & Time: {file_info.date_time}")
+    print(f"\tThe compressed file is {round(file_info.file_size / file_info.compress_size, 2)}x smaller.")
 
 
 if __name__ == "__main__":
